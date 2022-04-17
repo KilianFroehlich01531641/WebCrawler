17b8004a65616d2cc091854bc1f06f7cc872eb4c
KilianFroehlich01531641 <kilianfr@edu.aau.at>
2022-04-17 18:15:27 +0200

    added functional parser methods and further tests

--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -1,13 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebCrawler {
 
-    private URL userURL;
-    private int userPageDepth;
+    int userMaximumPageDepth;
+    ArrayList<List> queueList;
+    ArrayList<String> depthLevelResults;
+
+    public WebCrawler(int userPageDepth, String userURL){
+        this.userMaximumPageDepth = userPageDepth;
+        queueList = new ArrayList<>(userPageDepth);
+        depthLevelResults = new ArrayList<>(userPageDepth);
+        for (int i = 0; i < userPageDepth; i++) {
+            queueList.add(new ArrayList<URL>());
+            depthLevelResults.add("");
+        }
+
+        /*createMDFile();
+        writeMDFile("<br>depth: "+ this.userPageDepth +"\n" +
+                "<br>source language: english\n" +
+                "<br>target language: german\n" +
+                "<br>summary: ");
+         */
+        try{
+            queueList.get(0).add(new URL (userURL));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
     public String getRawHTMLFromURL(URL userURL){
@@ -21,25 +47,72 @@ public class WebCrawler {
             }while (currentLineInHMTL != null);
         } catch (Exception e){
             e.printStackTrace();
+            return rawHTMLPage;
         }
         return rawHTMLPage;
     }
-    public void writeToMDFile(){
 
+    public boolean createMDFile(){
+        File resultFile = new File("result_"+ userMaximumPageDepth + ".md");
+        try{
+            if(resultFile.createNewFile()){
+                System.out.println("File Created.");
+                return true;
+            }else {
+                System.out.println("File exists already.");
+                return false;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
+    public void writeMDFile(String input){
+        try{
+            FileWriter resultFile = new FileWriter("result_"+ userMaximumPageDepth + ".md");
+            resultFile.write(input);
+            resultFile.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
-    public void parsingForLinksInString(String rawHTMLPage){
+    public void parsingForLinksInString(String rawHTMLPage, int currentDepth, URL currentURL){
+        queueList.get(currentDepth).remove(currentURL);
+        if(currentDepth+1 >= userMaximumPageDepth){
+            return;
+        }
+        if(rawHTMLPage == ""){
+            depthLevelResults.set(currentDepth, depthLevelResults.get(currentDepth) + "<br>--> broken link to <a>" + currentURL.toString() +"</a>\n\n");
+            return;
+        }else{
+            depthLevelResults.set(currentDepth, depthLevelResults.get(currentDepth) + "<br>--> link to <a>" + currentURL.toString() +"</a>\n");
+            parsingForHeadersInString(rawHTMLPage, currentDepth);
+        }
+
         String urlPattern = "(www|http:|https:)+[^\\s]+[\\w]";
         Pattern pattern = Pattern.compile(urlPattern);
         Matcher matcher = pattern.matcher(rawHTMLPage);
 
-        while (matcher.find()){
-            String subURL = matcher.group();
+        try{
+            while (matcher.find()){
+                queueList.get(currentDepth+1).add(new URL(matcher.group()));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
-    public void parsingForHeadersInString(String rawHTMLPage){
+    public void parsingForHeadersInString(String rawHTMLPage, int currentDepth){
+        String headerPattern = "(<h[1-6]>)[\\w]+(</h[1-6]>)";
+        Pattern pattern = Pattern.compile(headerPattern);
+        Matcher matcher = pattern.matcher(rawHTMLPage);
+        String headerString = "";
 
+        while (matcher.find()){
+            headerString += matcher.group() + "\n";
+        }
+        depthLevelResults.set(currentDepth, depthLevelResults.get(currentDepth) + headerString +"\n");
     }
 }
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,14 +1,22 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
 public class TestClass {
 
-    private WebCrawler webCrawler = new WebCrawler();
+    private WebCrawler webCrawler;
+
+    @Before
+    public void setup(){
+        webCrawler = new WebCrawler(4, "http://www.google.at");
+    }
 
     @Test
     public void testingIfURLIsReadRight() throws MalformedURLException {
@@ -19,8 +27,65 @@ public class TestClass {
     }
 
     @Test(expected = Exception.class)
-    public void testingIfWrongURLIsHAndledRight() throws Exception{
-        webCrawler.getRawHTMLFromURL(new URL(""));
+    public void testingIfWrongURLIsHandledRight() throws Exception{
+        assertEquals("", webCrawler.getRawHTMLFromURL(new URL("")));
     }
+
+    @Test
+    public void creatingFileTest(){
+        try{
+            //creating file first time
+            assertTrue(webCrawler.createMDFile());
+            //creating it second time
+            assertFalse(webCrawler.createMDFile());
+        }finally {
+            //deleting the file so other test are not affected
+            File testfile = new File("result_"+ webCrawler.userMaximumPageDepth + ".md");
+            testfile.delete();
+        }
+    }
+
+    @Test
+    public void writeToFileTest(){
+        String teststring = "Testing the method345@//##?._ .";
+        webCrawler.createMDFile();
+        webCrawler.writeMDFile(teststring);
+        try{
+            File testFile = new File("result_"+ webCrawler.userMaximumPageDepth + ".md");
+            Scanner scanner = new Scanner(testFile);
+            assertEquals(teststring, scanner.nextLine());
+            scanner.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            File testFile = new File("result_"+ webCrawler.userMaximumPageDepth + ".md");
+            testFile.delete();
+        }
+    }
+
+    @Test
+    public void lookingForHeaders(){
+        try{
+            webCrawler.parsingForHeadersInString(webCrawler.getRawHTMLFromURL(new URL("https://www.w3schools.com/tags/tag_header.asp")), 0);
+            assertEquals("<h3>Example</h3>\n" +
+                    "<h3>Example</h3>\n" +
+                    "<h4>Exercises</h4>\n" +
+                    "\n", webCrawler.depthLevelResults.get(0));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void lookingForHeadersInEmptyHTML(){
+        try{
+            webCrawler.parsingForHeadersInString("", 0);
+            assertEquals("\n", webCrawler.depthLevelResults.get(0));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
 
